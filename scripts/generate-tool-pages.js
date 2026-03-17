4dd34a0d01258865bafa394f737df59aad2616f2
youssef <yousseflahsaniiiiii@gmail.com>
2026-03-17 19:52:49 +0000

    Use AW loader tag sitewide and keep GA config

--- scripts/generate-tool-pages.js
+++ scripts/generate-tool-pages.js
@@ -649,13 +649,13 @@ const pageTemplate = (tool) => {
   return `<!DOCTYPE html>
 <html lang="en">
 <head>
-  <script async src="https://www.googletagmanager.com/gtag/js?id=${GA_ID}"></script>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=${ADS_ID}"></script>
   <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', '${GA_ID}');
     gtag('config', '${ADS_ID}');
+    gtag('config', '${GA_ID}');
   </script>
   <script>
     (function () {
@@ -790,13 +790,13 @@ const indexTemplate = () => {
   return `<!DOCTYPE html>
 <html lang="en">
 <head>
-  <script async src="https://www.googletagmanager.com/gtag/js?id=${GA_ID}"></script>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=${ADS_ID}"></script>
   <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', '${GA_ID}');
     gtag('config', '${ADS_ID}');
+    gtag('config', '${GA_ID}');
   </script>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
